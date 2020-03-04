2f87b34816a6a068e8ab05ac7c922f06fb81d19c
Wasim <mohammed.wasimuddin@payback.net>
2020-03-04 13:59:53 +0100

    Move responsibility to drawer

--- Pinpad/PinPad/DrawerViewController.swift
+++ Pinpad/PinPad/DrawerViewController.swift
@@ -5,7 +5,6 @@ enum DrawerState {
     case closed
     case open
 
-    // simplify this
     var opposite: DrawerState {
         switch self {
         case .open: return .closed
@@ -16,6 +15,10 @@ enum DrawerState {
 
 class DrawerViewController: UIViewController {
 
+    private var animationProgress: CGFloat = 0
+    private var runningAnimator: UIViewPropertyAnimator!
+    private var bottomConstraint: NSLayoutConstraint?
+
     var drawerCurrentState: DrawerState = .open
 
     let drawerContentHolderView: DrawerHitAreaView = {
@@ -23,6 +26,10 @@ class DrawerViewController: UIViewController {
         view.backgroundColor = .white
         return view
     }()
+
+    private lazy var drawerOffset: CGFloat = {
+           return self.view.frame.height - 200
+       }()
 
     override func loadView() {
         super.loadView()
@@ -33,6 +40,8 @@ class DrawerViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupSubView()
+
+        setupConstraints()
     }
 
     private func setupRoundedCorners(for view: UIView) {
@@ -43,16 +52,104 @@ class DrawerViewController: UIViewController {
     private func setupSubView() {
         view.addSubview(drawerContentHolderView)
         setupRoundedCorners(for: drawerContentHolderView)
+
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePan(recognizer:)))
+        view.addGestureRecognizer(panGesture)
     }
 
     private func setupConstraints() {
         drawerContentHolderView.pinLeadingAndTrailingEdges(to: view)
-        drawerContentHolderView.pinBottomEdge(greaterThanOrEqualTo: view)
+        drawerContentHolderView.pinHeight(to: view.bounds.height - 110)
+        bottomConstraint =  drawerContentHolderView.pinBottomEdge(to: view, withOffset: drawerOffset)
+
     }
 
     func removeFromFaceOfEarth() {
         willMove(toParent: nil)
         view.removeFromSuperview()
         removeFromParent()
+    }
+}
+
+// Dedicated for Animation
+extension DrawerViewController {
+    func animateTransitionIfNeeded(to state: DrawerState, duration: TimeInterval = 0.5) {
+        let transitionAnimator = UIViewPropertyAnimator(duration: duration, dampingRatio: 1, animations: {
+            switch state {
+                case .open:
+                    self.bottomConstraint?.constant = 0
+                    self.view.backgroundColor = UIColor.black.withAlphaComponent(0.5)
+                case .closed:
+                    self.bottomConstraint?.constant = self.drawerOffset
+                    self.view.backgroundColor = .clear
+            }
+            self.view.layoutIfNeeded()
+        })
+
+        transitionAnimator.addCompletion { position in
+            switch position {
+                case .start:
+                    self.drawerCurrentState = state.opposite
+                case .end:
+                    self.drawerCurrentState = state
+                default:
+                    ()
+            }
+
+            switch self.drawerCurrentState {
+                case .open:
+                    self.bottomConstraint?.constant = 0
+                case .closed:
+                    self.bottomConstraint?.constant = self.drawerOffset
+            }
+
+        }
+
+        transitionAnimator.startAnimation()
+
+        runningAnimator = transitionAnimator
+    }
+
+    @objc private func handlePan(recognizer: UIPanGestureRecognizer) {
+        switch recognizer.state {
+            case .began:
+                animateTransitionIfNeeded(to: drawerCurrentState.opposite)
+                runningAnimator.pauseAnimation()
+                animationProgress = runningAnimator.fractionComplete
+
+            case .changed:
+                let translation = recognizer.translation(in: drawerContentHolderView)
+                var fraction = -translation.y / drawerOffset
+
+                if drawerCurrentState == .open { fraction *= -1 }
+                if runningAnimator.isReversed { fraction *= -1 }
+                runningAnimator.fractionComplete = fraction + animationProgress
+
+            case .ended:
+                let yVelocity = recognizer.velocity(in: drawerContentHolderView).y
+                let shouldClose = yVelocity > 0
+
+                // Early exit if no motion
+                if yVelocity == 0 {
+                    runningAnimator.continueAnimation(withTimingParameters: nil, durationFactor: 0)
+                    break
+                }
+
+                // Reserve animations
+                switch drawerCurrentState {
+                    case .open:
+                        if !shouldClose && !runningAnimator.isReversed { runningAnimator.isReversed = !runningAnimator.isReversed }
+                        if shouldClose && runningAnimator.isReversed { runningAnimator.isReversed = !runningAnimator.isReversed }
+                    case .closed:
+                        if shouldClose && !runningAnimator.isReversed { runningAnimator.isReversed = !runningAnimator.isReversed }
+                        if !shouldClose && runningAnimator.isReversed { runningAnimator.isReversed = !runningAnimator.isReversed }
+                }
+
+                // continue with animation
+                runningAnimator.continueAnimation(withTimingParameters: nil, durationFactor: 0)
+
+            default:
+                break
+        }
     }
 }
--- Pinpad/PinPad/ViewController.swift
+++ Pinpad/PinPad/ViewController.swift
@@ -6,14 +6,6 @@ class ViewController: UIViewController {
 
     // MARK: - Constants
 
-    private lazy var drawerOffset: CGFloat = {
-        return self.view.frame.height
-    }()
-
-    private var bottomConstraint: NSLayoutConstraint?
-    private var animationProgress: CGFloat = 0
-    private var runningAnimator: UIViewPropertyAnimator!
-
     private let button: UIButton = {
         let button = UIButton(type: .system)
         button.setTitle("Show Drawer", for: .normal)
@@ -52,25 +44,22 @@ class ViewController: UIViewController {
 
     @objc private func detectTap(gestureRecognizer: UITapGestureRecognizer) {
         guard drawerViewController.drawerCurrentState == .open else { return }
-        print("tap detected")
+
         // only accept touches in transparent View location
         let location = gestureRecognizer.location(in: view)
         let drawView = drawerViewController.drawerContentHolderView.frame
 
         guard location.y < drawView.origin.y else { return }
 
-        //        animateCard(with: view.bounds.height - 150)
-        animateTransitionIfNeeded(to: drawerViewController.drawerCurrentState.opposite)
-
+        drawerViewController.animateTransitionIfNeeded(to: drawerViewController.drawerCurrentState.opposite)
         drawerViewController.drawerCurrentState = .open
     }
 
     @objc func setupAndShowDrawer() {
         setupDrawerController()
-        setupDrawerConstraints()
 
         DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) { [weak self] in
-            self?.animateTransitionIfNeeded(to: DrawerState.open)
+            self?.drawerViewController.animateTransitionIfNeeded(to: DrawerState.open)
         }
     }
 
@@ -102,97 +91,6 @@ class ViewController: UIViewController {
         
         drawerViewController.didMove(toParent: self)
         drawerViewController.drawerCurrentState = .closed
-
-        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePan(recognizer:)))
-        drawerViewController.view.addGestureRecognizer(panGesture)
-
-    }
-
-    private func setupDrawerConstraints() {
-        drawerViewController.drawerContentHolderView.pinLeadingAndTrailingEdges(to: view)
-        drawerViewController.drawerContentHolderView.pinHeight(to: view.bounds.height - 110)
-        bottomConstraint =  drawerViewController.drawerContentHolderView.pinBottomEdge(to: view, withOffset: drawerOffset)
-
-    }
-
-    @objc private func handlePan(recognizer: UIPanGestureRecognizer) {
-        switch recognizer.state {
-            case .began:
-                animateTransitionIfNeeded(to: drawerViewController.drawerCurrentState.opposite)
-                runningAnimator.pauseAnimation()
-                animationProgress = runningAnimator.fractionComplete
-
-            case .changed:
-                let translation = recognizer.translation(in: drawerViewController.drawerContentHolderView)
-                var fraction = -translation.y / drawerOffset
-
-                if drawerViewController.drawerCurrentState == .open { fraction *= -1 }
-                if runningAnimator.isReversed { fraction *= -1 }
-                runningAnimator.fractionComplete = fraction + animationProgress
-
-            case .ended:
-                let yVelocity = recognizer.velocity(in: drawerViewController.drawerContentHolderView).y
-                let shouldClose = yVelocity > 0
-
-                // Early exit if no motion
-                if yVelocity == 0 {
-                    runningAnimator.continueAnimation(withTimingParameters: nil, durationFactor: 0)
-                    break
-                }
-
-                // Reserve animations
-                switch drawerViewController.drawerCurrentState {
-                    case .open:
-                        if !shouldClose && !runningAnimator.isReversed { runningAnimator.isReversed = !runningAnimator.isReversed }
-                        if shouldClose && runningAnimator.isReversed { runningAnimator.isReversed = !runningAnimator.isReversed }
-                    case .closed:
-                        if shouldClose && !runningAnimator.isReversed { runningAnimator.isReversed = !runningAnimator.isReversed }
-                        if !shouldClose && runningAnimator.isReversed { runningAnimator.isReversed = !runningAnimator.isReversed }
-                }
-
-                // continue with animation
-                runningAnimator.continueAnimation(withTimingParameters: nil, durationFactor: 0)
-
-            default:
-                break
-        }
-    }
-
-    private func animateTransitionIfNeeded(to state: DrawerState, duration: TimeInterval = 0.5) {
-        let transitionAnimator = UIViewPropertyAnimator(duration: duration, dampingRatio: 1, animations: {
-            switch state {
-                case .open:
-                    self.bottomConstraint?.constant = 0
-                    self.drawerViewController.view.backgroundColor = UIColor.black.withAlphaComponent(0.5)
-                case .closed:
-                    self.bottomConstraint?.constant = self.drawerOffset
-                    self.drawerViewController.view.backgroundColor = .clear
-            }
-            self.view.layoutIfNeeded()
-        })
-
-        transitionAnimator.addCompletion { position in
-            switch position {
-                case .start:
-                    self.drawerViewController.drawerCurrentState = state.opposite
-                case .end:
-                    self.drawerViewController.drawerCurrentState = state
-                default:
-                    ()
-            }
-
-            switch self.self.drawerViewController.drawerCurrentState {
-                case .open:
-                    self.bottomConstraint?.constant = 0
-                case .closed:
-                    self.bottomConstraint?.constant = self.drawerOffset
-            }
-
-        }
-
-        transitionAnimator.startAnimation()
-
-        runningAnimator = transitionAnimator
     }
 }
 
